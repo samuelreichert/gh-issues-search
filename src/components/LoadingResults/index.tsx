d61fd68622d85b1ebbd224614c69303a51d0f3e4
Samuel Reichert <samuel.reichert@helloprint.com>
2021-12-12 21:10:02 +0100

    Create LoadingResults component to show when query is loading

---
+++ src/components/LoadingResults/index.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const LoadingResults = () => {
+  return (
+    <div className='loading-container'>
+      <h1 className='loading-title'>Loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingResults;
