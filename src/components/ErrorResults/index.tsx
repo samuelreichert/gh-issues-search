30dc96fa1d371790de3f1d2d7d50bbb3fbaa2f9d
Samuel Reichert <samuel.reichert@helloprint.com>
2021-12-12 21:09:40 +0100

    Create ErrorResults component to show when query returns error

---
+++ src/components/ErrorResults/index.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const ErrorResults = () => {
+  return (
+    <div className='error-container'>
+      <h2 className='error-title'>Error! Unable to find searched organization or repository</h2>
+    </div>
+  );
+};
+
+export default ErrorResults;
